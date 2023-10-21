04d6c8d21bdd99b0e38daad7348536de7968b499
Franco Libertini <libertini.f.a@gmail.com>
2023-10-21 02:14:58 -0300

    Actualizacion con lo pedido en el Trabajo Practico Js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+// Cambio de Modo Claro a Oscuro y viceversa 
+
+const botonAccesibilidad = document.getElementById("botonAccesibilidad");
+
+
+botonAccesibilidad.addEventListener("click", function () {
+   
+    document.body.classList.toggle("black-background");
+});
+
+// Alerta de Datos Del Desarrollador
+
+const botonDatosDesarrollador = document.getElementById("datosDesarrollador");
+
+
+botonDatosDesarrollador.addEventListener("click", function () {
+    const datosDelDesarrollador = "Datos Del Desarrollador:\n\nFranco Libertini\nLibertini.f.a@gmail.com\n2915747629";
+    alert(datosDelDesarrollador);
+});
