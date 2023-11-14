cc738c654f29814846a714c67355c4242271b643
Franco Libertini <libertini.f.a@gmail.com>
2023-11-14 10:11:54 -0300

    Saque los carteles de alert del modo nocturno y del inicio de carga

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ const botonAccesibilidad = document.getElementById("botonAccesibilidad");
 botonAccesibilidad.addEventListener("click", function () {
 
     document.body.classList.toggle("black-background");
-    alert ("anda")
 });
 
 // Alerta de Datos Del Desarrollador
